67f8fe205f10b149483c4cfc1b636ab1b7f5f154
ApoloGH <apolo-gh@users.noreply.github.com>
2021-04-21 20:12:15 -0400

    refactor: remove console logs

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
 import { performance as perf } from 'perf_hooks';
 
-if (typeof performance === 'undefined') {
-  console.log('node performance', perf);
-} else {
-  console.log('web performance', perf);
-}
-
 export default typeof performance === 'undefined' ? perf : performance;
