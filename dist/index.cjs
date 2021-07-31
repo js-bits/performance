6695dede56fcc354301ea3278b642dcaf5cdf157
ApoloGH <apolo-gh@users.noreply.github.com>
2021-07-30 22:22:22 -0400

    chore: remove console log

--- dist/index.cjs
+++ dist/index.cjs
@@ -2,8 +2,6 @@
 
 var perf_hooks = require('perf_hooks');
 
-console.log(`performance import mode ${typeof require === 'undefined' ? 'esm' : 'cjs'}`);
-
 const returnValue = typeof performance === 'undefined' ? perf_hooks.performance : performance;
 
 module.exports = returnValue;
--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import { performance as perf } from 'perf_hooks';
 
-console.log(`performance import mode ${typeof require === 'undefined' ? 'esm' : 'cjs'}`);
-
 const returnValue = typeof performance === 'undefined' ? perf : performance;
 
 export default returnValue;
