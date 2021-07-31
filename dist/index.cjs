7ec502df9cb64a1143cfa902e1299b401206730d
ApoloGH <apolo-gh@users.noreply.github.com>
2021-07-30 22:06:50 -0400

    chore: add console log

--- dist/index.cjs
+++ dist/index.cjs
@@ -2,6 +2,8 @@
 
 var perf_hooks = require('perf_hooks');
 
+console.log(`log-in-color import mode ${typeof require === 'undefined' ? 'esm' : 'cjs'}`);
+
 const returnValue = typeof performance === 'undefined' ? perf_hooks.performance : performance;
 
 module.exports = returnValue;
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import { performance as perf } from 'perf_hooks';
 
+console.log(`log-in-color import mode ${typeof require === 'undefined' ? 'esm' : 'cjs'}`);
+
 const returnValue = typeof performance === 'undefined' ? perf : performance;
 
 export default returnValue;
