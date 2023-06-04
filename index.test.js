c6a5f99196e012757fe3fc27e3f78a8257ec5864
ApoloGH <apolo-gh@users.noreply.github.com>
2023-06-04 17:11:35 -0400

    refactor: fix linting

--- index.test.js
+++ index.test.js
@@ -2,7 +2,7 @@ import performance from './index.js';
 // import performance from './dist/index.cjs';
 // const performance = require('./dist/index.cjs');
 
-describe(`performance`, () => {
+describe('performance', () => {
   test('should be defined', () => {
     expect(performance).toBeDefined();
   });
